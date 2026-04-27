84efbcac2973f29ef0c8b8fb3322efa47343b44a
Bruno Kenji <bruno.168899-2024@aluno.unicv.edu.br>
2026-04-27 19:54:41 -0300

    Cadastro_Clientes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import RotaProtegida from "./components/RotaProtegida";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import CadastroCliente from "./pages/CadastroCliente";
+
+function CadastroClienteWrapper() {
+  const navigate = useNavigate();
+  return <CadastroCliente onCancelar={() => navigate(-1)} />;
+}
 
 function App() {
   return (
@@ -8,6 +16,22 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <RotaProtegida>
+                <Dashboard />
+              </RotaProtegida>
+            }
+          />
+          <Route
+            path="/clientes/cadastro"
+            element={
+              <RotaProtegida>
+                <CadastroClienteWrapper />
+              </RotaProtegida>
+            }
+          />
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
